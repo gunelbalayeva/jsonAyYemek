4ac3dfa74a44caac79ef59a41eab0ab6a54feec6
gunelbalayeva <gunelbalayeva97@gmail.com>
2025-03-14 19:26:30 +0400

    Initial Commit

---
+++ 14.03.practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  14.03.practice
+//
+//  Created by User on 14.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
